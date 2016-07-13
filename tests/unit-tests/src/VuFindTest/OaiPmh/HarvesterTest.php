09859836dddf254648101195c03cdd7570a13f8f
Demian Katz <demian.katz@villanova.edu>
2016-07-13 14:17:17 -0400

    Improved tests.

--- tests/unit-tests/src/VuFindTest/OaiPmh/HarvesterTest.php
+++ tests/unit-tests/src/VuFindTest/OaiPmh/HarvesterTest.php
@@ -78,6 +78,25 @@ class HarvesterTest extends \PHPUnit_Framework_TestCase
     }
 
     /**
+     * Return protected or private property.
+     *
+     * Uses PHP's reflection API in order to modify property accessibility.
+     *
+     * @param object|string $object   Object or class name
+     * @param string        $property Property name
+     *
+     * @throws \ReflectionException Property does not exist
+     *
+     * @return mixed
+     */
+    protected function getProperty($object, $property)
+    {
+        $reflectionProperty = new \ReflectionProperty($object, $property);
+        $reflectionProperty->setAccessible(true);
+        return $reflectionProperty->getValue($object);
+    }
+
+    /**
      * Get harvester
      *
      * @param array  $settings     Settings
@@ -128,6 +147,17 @@ class HarvesterTest extends \PHPUnit_Framework_TestCase
 </OAI-PMH>
 XML;
     }
+
+    protected function getTokenErrorResponse()
+    {
+        return <<<XML
+<?xml version="1.0"?>
+<OAI-PMH xmlns="http://www.openarchives.org/OAI/2.0/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.openarchives.org/OAI/2.0/ http://www.openarchives.org/OAI/2.0/OAI-PMH.xsd"><responseDate>2016-07-13T14:11:24Z</responseDate><request verb="ListRecords" resumptionToken="foo">http://fake/OAI/Server</request>
+<error code="badResumptionToken">Invalid or expired resumption token</error>
+</OAI-PMH>
+XML;
+    }
+
     /**
      * Get XML ListRecords response for testing.
      *
@@ -157,25 +187,74 @@ XML;
     }
 
     /**
-     * Test basic harvest
+     * Test that granularity is autoloaded by default.
      *
      * @return void
      */
-    public function testHarvest()
+    public function testGranularityAutoload()
     {
         $comm = $this->getMockCommunicator();
-        $expectedSettings = ['metadataPrefix' => 'oai_dc'];
-        $comm->expects($this->at(0))->method('request')
+        $comm->expects($this->once(0))->method('request')
             ->with($this->equalTo('Identify'))
             ->will(
                 $this->returnValue(
                     simplexml_load_string($this->getFakeIdentifyResponse())
                 )
             );
-        $comm->expects($this->at(1))->method('request')
+        $harvester = $this->getHarvester([], $comm);
+        $this->assertEquals(
+            'YYYY-MM-DDThh:mm:ssZ', $this->getProperty($harvester, 'granularity')
+        );
+    }
+
+    /**
+     * Test that a single ListRecords call with no resumption token triggers
+     * a write to the writer.
+     *
+     * @return void
+     */
+    public function testSimpleListRecords()
+    {
+        $comm = $this->getMockCommunicator();
+        $expectedSettings = ['metadataPrefix' => 'oai_dc'];
+        $comm->expects($this->once())->method('request')
             ->with($this->equalTo('ListRecords'), $this->equalTo($expectedSettings))
             ->will($this->returnValue(simplexml_load_string($this->getFakeResponse())));
-        $harvester = $this->getHarvester([], $comm);
+        $writer = $this->getMockRecordWriter();
+        $writer->expects($this->once())->method('write')
+            ->with($this->isInstanceOf('SimpleXMLElement'));
+        $harvester = $this->getHarvester(
+            ['dateGranularity' => 'YYYY-MM-DDThh:mm:ssZ'], $comm, $writer
+        );
+        $harvester->launch();
+    }
+
+    /**
+     * Test a bad resumption token error.
+     *
+     * @return void
+     *
+     * @expectedException        Exception
+     * @expectedExceptionMessage Token expired; removing last_state.txt. Please restart harvest.
+     */
+    public function testBadResumptionToken()
+    {
+        $comm = $this->getMockCommunicator();
+        $expectedSettings = ['resumptionToken' => 'foo'];
+        $comm->expects($this->once())->method('request')
+            ->with($this->equalTo('ListRecords'), $this->equalTo($expectedSettings))
+            ->will(
+                $this->returnValue(
+                    simplexml_load_string($this->getTokenErrorResponse())
+                )
+            );
+        $sm = $this->getMockStateManager();
+        $sm->expects($this->any())->method('loadState')
+            ->will($this->returnValue([null, 'foo', 'bar']));
+        $sm->expects($this->once())->method('clearState');
+        $harvester = $this->getHarvester(
+            ['dateGranularity' => 'YYYY-MM-DDThh:mm:ssZ'], $comm, null, $sm
+        );
         $harvester->launch();
     }
 }
