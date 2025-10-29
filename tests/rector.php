c5fd072e53ec0cd8d801a490f5734980292fc9e3
Demian Katz <demian.katz@villanova.edu>
2025-10-29 08:46:26 -0400

    Add Rector to project tooling. (#36)

---
+++ tests/rector.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Caching\ValueObject\Storage\FileCacheStorage;
+use Rector\Config\RectorConfig;
+
+return RectorConfig::configure()
+    ->withCache(
+        cacheClass: FileCacheStorage::class,
+        cacheDirectory: __DIR__ . '/../.rector'
+    )->withPaths([
+        __DIR__ . '/../src',
+        __DIR__ . '/../tests',
+    ])
+    ->withTypeCoverageLevel(0)
+    ->withDeadCodeLevel(0)
+    ->withCodeQualityLevel(0);
